a06bc77e56443e8e292429693eb58db9060d3121
ThanhGit <vodiepthanh06@gmail.com>
2025-10-07 18:37:04 +0700

    abc

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace signin_signup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblht = new System.Windows.Forms.Label();
+            this.lble = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labmk = new System.Windows.Forms.Label();
+            this.labnlmk = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblsdt = new System.Windows.Forms.Label();
+            this.labns = new System.Windows.Forms.Label();
+            this.textht = new System.Windows.Forms.TextBox();
+            this.texte = new System.Windows.Forms.TextBox();
+            this.textns = new System.Windows.Forms.TextBox();
+            this.textmk = new System.Windows.Forms.TextBox();
+            this.textsdt = new System.Windows.Forms.TextBox();
+            this.textnlmk = new System.Windows.Forms.TextBox();
+            this.btndk = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblht
+            // 
+            this.lblht.AutoSize = true;
+            this.lblht.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblht.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblht.Location = new System.Drawing.Point(29, 101);
+            this.lblht.Name = "lblht";
+            this.lblht.Size = new System.Drawing.Size(94, 26);
+            this.lblht.TabIndex = 0;
+            this.lblht.Text = "Họ  tên :";
+            this.lblht.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lble
+            // 
+            this.lble.AutoSize = true;
+            this.lble.Location = new System.Drawing.Point(29, 201);
+            this.lble.Name = "lble";
+            this.lble.Size = new System.Drawing.Size(87, 26);
+            this.lble.TabIndex = 1;
+            this.lble.Text = "E-mail :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 26);
+            this.label3.TabIndex = 2;
+            // 
+            // labmk
+            // 
+            this.labmk.AutoSize = true;
+            this.labmk.Location = new System.Drawing.Point(29, 301);
+            this.labmk.Name = "labmk";
+            this.labmk.Size = new System.Drawing.Size(113, 26);
+            this.labmk.TabIndex = 3;
+            this.labmk.Text = "Mật khẩu :";
+            this.labmk.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // labnlmk
+            // 
+            this.labnlmk.AutoSize = true;
+            this.labnlmk.Location = new System.Drawing.Point(29, 351);
+            this.labnlmk.Name = "labnlmk";
+            this.labnlmk.Size = new System.Drawing.Size(200, 26);
+            this.labnlmk.TabIndex = 4;
+            this.labnlmk.Text = "Nhập lại mật khẩu :";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(16, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(612, 41);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ĐĂNG KÝ TÀI KHOẢN ";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblsdt
+            // 
+            this.lblsdt.AutoSize = true;
+            this.lblsdt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsdt.Location = new System.Drawing.Point(29, 151);
+            this.lblsdt.Name = "lblsdt";
+            this.lblsdt.Size = new System.Drawing.Size(151, 26);
+            this.lblsdt.TabIndex = 7;
+            this.lblsdt.Text = "Số điện thoại :";
+            this.lblsdt.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // labns
+            // 
+            this.labns.AutoSize = true;
+            this.labns.Location = new System.Drawing.Point(23, 251);
+            this.labns.Name = "labns";
+            this.labns.Size = new System.Drawing.Size(127, 26);
+            this.labns.TabIndex = 8;
+            this.labns.Text = "Ngày sinh : ";
+            this.labns.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // textht
+            // 
+            this.textht.BackColor = System.Drawing.Color.White;
+            this.textht.Location = new System.Drawing.Point(243, 101);
+            this.textht.Margin = new System.Windows.Forms.Padding(2);
+            this.textht.Name = "textht";
+            this.textht.Size = new System.Drawing.Size(344, 32);
+            this.textht.TabIndex = 9;
+            this.textht.TextChanged += new System.EventHandler(this.textht_TextChanged);
+            // 
+            // texte
+            // 
+            this.texte.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.texte.Location = new System.Drawing.Point(243, 201);
+            this.texte.Margin = new System.Windows.Forms.Padding(2);
+            this.texte.Name = "texte";
+            this.texte.Size = new System.Drawing.Size(344, 32);
+            this.texte.TabIndex = 10;
+            // 
+            // textns
+            // 
+            this.textns.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textns.Location = new System.Drawing.Point(243, 251);
+            this.textns.Margin = new System.Windows.Forms.Padding(2);
+            this.textns.Name = "textns";
+            this.textns.Size = new System.Drawing.Size(344, 32);
+            this.textns.TabIndex = 12;
+            // 
+            // textmk
+            // 
+            this.textmk.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textmk.Location = new System.Drawing.Point(243, 301);
+            this.textmk.Margin = new System.Windows.Forms.Padding(2);
+            this.textmk.Name = "textmk";
+            this.textmk.PasswordChar = '*';
+            this.textmk.Size = new System.Drawing.Size(344, 32);
+            this.textmk.TabIndex = 13;
+            this.textmk.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textsdt
+            // 
+            this.textsdt.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textsdt.Location = new System.Drawing.Point(243, 151);
+            this.textsdt.Margin = new System.Windows.Forms.Padding(2);
+            this.textsdt.Name = "textsdt";
+            this.textsdt.Size = new System.Drawing.Size(344, 32);
+            this.textsdt.TabIndex = 14;
+            this.textsdt.TextChanged += new System.EventHandler(this.textsdt_TextChanged);
+            // 
+            // textnlmk
+            // 
+            this.textnlmk.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textnlmk.Location = new System.Drawing.Point(243, 351);
+            this.textnlmk.Margin = new System.Windows.Forms.Padding(2);
+            this.textnlmk.Name = "textnlmk";
+            this.textnlmk.PasswordChar = '*';
+            this.textnlmk.Size = new System.Drawing.Size(344, 32);
+            this.textnlmk.TabIndex = 15;
+            // 
+            // btndk
+            // 
+            this.btndk.BackColor = System.Drawing.Color.Blue;
+            this.btndk.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btndk.Location = new System.Drawing.Point(268, 398);
+            this.btndk.Margin = new System.Windows.Forms.Padding(2);
+            this.btndk.Name = "btndk";
+            this.btndk.Size = new System.Drawing.Size(187, 49);
+            this.btndk.TabIndex = 16;
+            this.btndk.Text = "đăng ký";
+            this.btndk.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(176, 451);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 57);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "[đăng nhập]";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 470);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(159, 26);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "đã có tài khoản";
+            this.label7.Click += new System.EventHandler(this.label7_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(640, 519);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btndk);
+            this.Controls.Add(this.textnlmk);
+            this.Controls.Add(this.textsdt);
+            this.Controls.Add(this.textmk);
+            this.Controls.Add(this.textns);
+            this.Controls.Add(this.texte);
+            this.Controls.Add(this.textht);
+            this.Controls.Add(this.labns);
+            this.Controls.Add(this.lblsdt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.labnlmk);
+            this.Controls.Add(this.labmk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lble);
+            this.Controls.Add(this.lblht);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "sign up";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblht;
+        private System.Windows.Forms.Label lble;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labmk;
+        private System.Windows.Forms.Label labnlmk;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblsdt;
+        private System.Windows.Forms.Label labns;
+        private System.Windows.Forms.TextBox textht;
+        private System.Windows.Forms.TextBox texte;
+        private System.Windows.Forms.TextBox textns;
+        private System.Windows.Forms.TextBox textmk;
+        private System.Windows.Forms.TextBox textsdt;
+        private System.Windows.Forms.TextBox textnlmk;
+        private System.Windows.Forms.Button btndk;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace signin_signup
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+            
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
